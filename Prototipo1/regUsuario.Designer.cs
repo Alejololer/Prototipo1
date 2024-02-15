b62b3481da57e5d29389b3bdf1edc4e5c4469353
ANTHONY JAVIER GOMEZ NINO <anthony.gomez@epn.edu.ec>
2024-02-15 15:48:49 -0500

    Ventana Registrar Usuario Listo

--- Prototipo1/regUsuario.Designer.cs
+++ Prototipo1/regUsuario.Designer.cs
@@ -43,19 +43,20 @@
             // 
             // button2
             // 
-            button2.Location = new Point(296, 196);
+            button2.Location = new Point(337, 258);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(75, 23);
+            button2.Size = new Size(86, 31);
             button2.TabIndex = 23;
             button2.Text = "Cancelar";
             button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(167, 196);
+            button1.Location = new Point(191, 258);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 22;
             button1.Text = "Registrar";
             button1.UseVisualStyleBackColor = true;
@@ -63,33 +64,35 @@
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(142, 79);
+            textBox2.Location = new Point(175, 113);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(229, 23);
+            textBox2.Size = new Size(248, 27);
             textBox2.TabIndex = 21;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(142, 46);
+            textBox1.Location = new Point(175, 65);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(229, 23);
+            textBox1.Size = new Size(248, 27);
             textBox1.TabIndex = 20;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(66, 82);
+            label3.Location = new Point(-1, 116);
             label3.Name = "label3";
-            label3.Size = new Size(70, 15);
+            label3.Size = new Size(86, 20);
             label3.TabIndex = 19;
             label3.Text = "Contraseña:";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(86, 49);
+            label2.Location = new Point(-1, 68);
             label2.Name = "label2";
-            label2.Size = new Size(50, 15);
+            label2.Size = new Size(62, 20);
             label2.TabIndex = 18;
             label2.Text = "Usuario:";
             // 
@@ -97,18 +100,18 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(109, 12);
             label1.Name = "label1";
-            label1.Size = new Size(180, 25);
+            label1.Size = new Size(225, 32);
             label1.TabIndex = 17;
-            label1.Text = "Datos del Paciente:";
+            label1.Text = "Datos del Paciente";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(44, 164);
+            label4.Location = new Point(-1, 213);
             label4.Name = "label4";
-            label4.Size = new Size(92, 15);
+            label4.Size = new Size(117, 20);
             label4.TabIndex = 24;
             label4.Text = "Tipo de Usuario:";
             // 
@@ -116,32 +119,34 @@
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Jefe de Laboratorio", "Asistente de Laboratorio", "Invitado" });
-            comboBox1.Location = new Point(142, 161);
+            comboBox1.Location = new Point(175, 209);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(229, 23);
+            comboBox1.Size = new Size(248, 28);
             comboBox1.TabIndex = 25;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(142, 123);
+            textBox3.Location = new Point(175, 161);
+            textBox3.Margin = new Padding(3, 4, 3, 4);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(229, 23);
+            textBox3.Size = new Size(248, 27);
             textBox3.TabIndex = 27;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(-1, 126);
+            label5.Location = new Point(-1, 164);
             label5.Name = "label5";
-            label5.Size = new Size(137, 15);
+            label5.Size = new Size(170, 20);
             label5.TabIndex = 26;
             label5.Text = "Confirmar la contraseña:";
             // 
             // regUsuario
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(387, 231);
+            ClientSize = new Size(442, 308);
             Controls.Add(textBox3);
             Controls.Add(label5);
             Controls.Add(comboBox1);
@@ -153,9 +158,9 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "regUsuario";
-            Text = "regUsuario";
-            Load += regUsuario_Load;
+            Text = "Registrar Usuario";
             ResumeLayout(false);
             PerformLayout();
         }
