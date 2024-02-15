e02671b49481f32634ffdcfcee1cd3b09b7fcb5b
ANTHONY JAVIER GOMEZ NINO <anthony.gomez@epn.edu.ec>
2024-02-15 15:52:21 -0500

    Ventana Resultados Lista

--- Prototipo1/Resultados.Designer.cs
+++ Prototipo1/Resultados.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label2 = new Label();
             panel1 = new Panel();
             label7 = new Label();
             label8 = new Label();
@@ -43,39 +42,31 @@
             panel1.SuspendLayout();
             SuspendLayout();
             // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Arial Narrow", 12F);
-            label2.Location = new Point(210, 185);
-            label2.Name = "label2";
-            label2.Size = new Size(0, 20);
-            label2.TabIndex = 47;
-            // 
             // panel1
             // 
             panel1.Controls.Add(label7);
             panel1.Controls.Add(label8);
-            panel1.Location = new Point(0, -2);
+            panel1.Location = new Point(-2, -3);
+            panel1.Margin = new Padding(3, 4, 3, 4);
             panel1.Name = "panel1";
-            panel1.Size = new Size(569, 25);
+            panel1.Size = new Size(650, 33);
             panel1.TabIndex = 54;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(32, 6);
+            label7.Location = new Point(-2, 3);
             label7.Name = "label7";
-            label7.Size = new Size(60, 15);
+            label7.Size = new Size(76, 20);
             label7.TabIndex = 55;
             label7.Text = "AmatreSis";
             // 
             // label8
             // 
             label8.AutoSize = true;
-            label8.Location = new Point(325, 6);
+            label8.Location = new Point(339, 3);
             label8.Name = "label8";
-            label8.Size = new Size(241, 15);
+            label8.Size = new Size(309, 20);
             label8.TabIndex = 22;
             label8.Text = "Usuario: Silvana Villegas - Jefe de laboratorio";
             // 
@@ -83,39 +74,40 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Arial Narrow", 12F);
-            label3.Location = new Point(414, 186);
+            label3.Location = new Point(526, 231);
             label3.Name = "label3";
-            label3.Size = new Size(134, 20);
+            label3.Size = new Size(91, 48);
             label3.TabIndex = 53;
-            label3.Text = "Modificar Resultados";
+            label3.Text = "Modificar \r\nResultados";
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial Narrow", 12F);
-            label1.Location = new Point(212, 186);
+            label1.Location = new Point(278, 231);
             label1.Name = "label1";
-            label1.Size = new Size(135, 20);
+            label1.Size = new Size(91, 48);
             label1.TabIndex = 52;
-            label1.Text = "Consultar Resultados";
+            label1.Text = "Consultar \r\nResultados";
             // 
             // label4
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Arial Narrow", 12F);
-            label4.Location = new Point(13, 186);
+            label4.Location = new Point(29, 231);
             label4.Name = "label4";
-            label4.Size = new Size(133, 20);
+            label4.Size = new Size(91, 48);
             label4.TabIndex = 51;
-            label4.Text = "Registrar Resultados";
+            label4.Text = "Registrar \r\nResultados";
             // 
             // button3
             // 
             button3.BackgroundImage = Properties.Resources.Resultados;
             button3.BackgroundImageLayout = ImageLayout.Zoom;
-            button3.Location = new Point(414, 49);
+            button3.Location = new Point(495, 52);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(134, 118);
+            button3.Size = new Size(153, 157);
             button3.TabIndex = 50;
             button3.UseVisualStyleBackColor = true;
             button3.Click += button3_Click;
@@ -124,9 +116,10 @@
             // 
             button2.BackgroundImage = Properties.Resources.Pedidos1;
             button2.BackgroundImageLayout = ImageLayout.Zoom;
-            button2.Location = new Point(213, 49);
+            button2.Location = new Point(247, 52);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(134, 118);
+            button2.Size = new Size(153, 157);
             button2.TabIndex = 49;
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
@@ -136,9 +129,10 @@
             button1.BackgroundImage = Properties.Resources.Cliente2;
             button1.BackgroundImageLayout = ImageLayout.Zoom;
             button1.ForeColor = SystemColors.ControlText;
-            button1.Location = new Point(12, 49);
+            button1.Location = new Point(-2, 52);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(134, 118);
+            button1.Size = new Size(153, 157);
             button1.TabIndex = 48;
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
@@ -147,9 +141,9 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Arial Narrow", 12F);
-            label5.Location = new Point(227, 362);
+            label5.Location = new Point(258, 480);
             label5.Name = "label5";
-            label5.Size = new Size(105, 20);
+            label5.Size = new Size(131, 24);
             label5.TabIndex = 56;
             label5.Text = "Generar Informe";
             // 
@@ -157,18 +151,19 @@
             // 
             button4.BackgroundImage = Properties.Resources.Pedidos1;
             button4.BackgroundImageLayout = ImageLayout.Zoom;
-            button4.Location = new Point(212, 225);
+            button4.Location = new Point(247, 301);
+            button4.Margin = new Padding(3, 4, 3, 4);
             button4.Name = "button4";
-            button4.Size = new Size(134, 118);
+            button4.Size = new Size(153, 157);
             button4.TabIndex = 55;
             button4.UseVisualStyleBackColor = true;
             button4.Click += button4_Click;
             // 
             // Resultados
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(565, 397);
+            ClientSize = new Size(646, 529);
             Controls.Add(label5);
             Controls.Add(button4);
             Controls.Add(panel1);
@@ -178,10 +173,9 @@
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(label2);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Resultados";
             Text = "Resultados";
-            Load += Resultados_Load;
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             ResumeLayout(false);
@@ -189,7 +183,6 @@
         }
 
         #endregion
-        private Label label2;
         private Panel panel1;
         private Label label8;
         private Label label3;
