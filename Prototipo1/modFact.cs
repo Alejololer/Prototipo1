70a51b0721ff4b32a657dd4574630c869e814e75
ANTHONY JAVIER GOMEZ NINO <anthony.gomez@epn.edu.ec>
2024-02-15 13:39:31 -0500

    Ventana Modificar Factura Listo

--- Prototipo1/modFact.cs
+++ Prototipo1/modFact.cs
@@ -24,10 +24,5 @@ namespace Prototipo1
             MessageBox.Show("Datos actualizados correctamente", "Actualización de Datos", MessageBoxButtons.OK, MessageBoxIcon.Information);
             this.Close();
         }
-
-        private void modFact_Load(object sender, EventArgs e)
-        {
-
-        }
     }
 }
