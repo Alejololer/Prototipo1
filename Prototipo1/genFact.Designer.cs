8774f404ddb684a9615b6ee9e9bd487b79287131
ANTHONY JAVIER GOMEZ NINO <anthony.gomez@epn.edu.ec>
2024-02-15 13:34:11 -0500

    Ventana Generar Factura del Pedido Lista

--- Prototipo1/genFact.Designer.cs
+++ Prototipo1/genFact.Designer.cs
@@ -36,9 +36,10 @@
             // 
             // button1
             // 
-            button1.Location = new Point(306, 81);
+            button1.Location = new Point(350, 108);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 33;
             button1.Text = "Generar";
             button1.UseVisualStyleBackColor = true;
@@ -48,17 +49,18 @@
             // 
             comboBox1.FormattingEnabled = true;
             comboBox1.Items.AddRange(new object[] { "Pedido realizado 1", "Pedidos realizado 2", "Pedido realizado 3" });
-            comboBox1.Location = new Point(79, 46);
+            comboBox1.Location = new Point(90, 62);
+            comboBox1.Margin = new Padding(3, 4, 3, 4);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(302, 23);
+            comboBox1.Size = new Size(345, 28);
             comboBox1.TabIndex = 32;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 49);
+            label2.Location = new Point(12, 66);
             label2.Name = "label2";
-            label2.Size = new Size(47, 15);
+            label2.Size = new Size(58, 20);
             label2.TabIndex = 31;
             label2.Text = "Pedido:";
             // 
@@ -66,24 +68,24 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(61, 12);
             label1.Name = "label1";
-            label1.Size = new Size(258, 25);
+            label1.Size = new Size(323, 32);
             label1.TabIndex = 30;
-            label1.Text = "Generar Factura del Pedido:";
+            label1.Text = "Generar Factura del Pedido";
             // 
             // genFact
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(389, 114);
+            ClientSize = new Size(445, 152);
             Controls.Add(button1);
             Controls.Add(comboBox1);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "genFact";
-            Text = "genFact";
-            Load += genFact_Load;
+            Text = "Generar Factura del Pedido";
             ResumeLayout(false);
             PerformLayout();
         }
