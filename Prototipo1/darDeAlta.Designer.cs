520763e5544561435af1038d37bf501f0ff058f1
ANTHONY JAVIER GOMEZ NINO <anthony.gomez@epn.edu.ec>
2024-02-15 13:17:32 -0500

    Ventana dar de alta paciente Lista

--- Prototipo1/darDeAlta.Designer.cs
+++ Prototipo1/darDeAlta.Designer.cs
@@ -36,36 +36,37 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(142, 46);
+            textBox1.Location = new Point(162, 69);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(228, 23);
+            textBox1.Size = new Size(260, 27);
             textBox1.TabIndex = 17;
-            textBox1.TextChanged += textBox1_TextChanged;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(20, 49);
+            label2.Location = new Point(9, 72);
             label2.Name = "label2";
-            label2.Size = new Size(116, 15);
+            label2.Size = new Size(147, 20);
             label2.TabIndex = 16;
-            label2.Text = "Cédula de Identidad:";            // 
+            label2.Text = "Cédula de Identidad:";
+            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(94, 12);
             label1.Name = "label1";
-            label1.Size = new Size(196, 25);
+            label1.Size = new Size(246, 32);
             label1.TabIndex = 15;
-            label1.Text = "Dar de Alta Paciente:";
-            label1.Click += label1_Click;
+            label1.Text = "Dar de Alta Paciente";
             // 
             // button1
             // 
-            button1.Location = new Point(153, 91);
+            button1.Location = new Point(174, 121);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 20;
             button1.Text = "Confirmar";
             button1.UseVisualStyleBackColor = true;
@@ -73,15 +74,16 @@
             // 
             // darDeAlta
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(380, 129);
+            ClientSize = new Size(434, 172);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "darDeAlta";
-            Text = "darDeAlta";
+            Text = "Dar de Alta a Paciente";
             ResumeLayout(false);
             PerformLayout();
         }
